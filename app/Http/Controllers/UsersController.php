205e3f84b84063aeb39cb048a4b4e0e5d917c69d
vagrant <vagrant@homestead>
2018-04-21 05:02:55 +0000

    添加密码重置和邮箱验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -82,12 +82,10 @@ class UsersController extends Controller
     protected function sendEmailConfirmationTo($user){
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'aufree@yousails.com';
-        $name = 'Aufree';
         $to = $user->email;
         $subject = "感谢您的注册，请确认你的邮箱";
-        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
